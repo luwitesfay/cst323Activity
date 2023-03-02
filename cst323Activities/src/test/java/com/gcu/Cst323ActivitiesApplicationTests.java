daccbc1c308e481b032782b4bcb1216fa02838ba
luwamtesfay <luwamtesfay@localhost>
2023-03-02 00:31:14 -0800

    First Commit

---
+++ cst323Activities/src/test/java/com/gcu/Cst323ActivitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cst323ActivitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
